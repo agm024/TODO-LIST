357c216963ad3a786a554015eb230be390d09bfb
agm024 <agastyagm2@gmail.com>
2024-09-10 19:45:26 +0000

    Auto Commit

--- app.py
+++ app.py
@@ -31,5 +31,5 @@ migrate = Migrate(app, db)
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(100), nullable=False)
-    password = 
+    username = db.Column(db.String(100), nullable=False, unique=)
+    email = db.Column(db.String(100), nullable=False)
